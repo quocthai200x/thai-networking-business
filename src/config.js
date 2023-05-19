60979a78eddbcaf28b5539c5d5b5fed25158c4c2
quocthai200x <quocthai2000x@gmail.com>
2023-05-19 22:00:12 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     rootPath: process.env.NODE_ENV === 'development'
-    ?'http://localhost:5555':'https://project-iii-back.vercel.app/'
+    ?'http://localhost:5555':'https://thai-networking-server.vercel.app/'
     // rootPath: 'http://localhost:6969'
 };
